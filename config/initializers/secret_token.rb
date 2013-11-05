12defc7ce4d7c9b92b82eaeb46b911af8d5a087e
Clee681 <christopher.lee681@gmail.com>
2013-11-05 10:37:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlatironSocialMedia::Application.config.secret_key_base = 'ecc632a61d95dfc26db0f64d4b157e19283fb91e6ae3e0896af2c07f0f489898326fdc6a908498a917c63a6e02c3b2ad0683ba7fb115b0cb894c0b6711120729'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlatironSocialMedia::Application.config.session_store :cookie_store, key: '_flatiron-social-media_session'
